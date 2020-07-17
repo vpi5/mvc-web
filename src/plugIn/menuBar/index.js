180ea45a382e6013f56962c5c2cba29cc52d4b9a
李晓松 <lixiaosong59@163.com>
2020-07-17 15:00:58 +0800

    修复旧的组件

--- src/plugIn/menuBar/index.js
+++ src/plugIn/menuBar/index.js
@@ -10,6 +10,7 @@ export default class MenuBar extends React.Component {
             menuArr : option.menuArr || [],
             menuAction : option.menuAction || '',
             forbidClick : option.forbidClick || [],
+            moldType : option.moldType || '',
         }
     }
 
@@ -25,7 +26,7 @@ export default class MenuBar extends React.Component {
 
     // 数据校验
     handleVerifyData = (data) => {
-        let {menuArr, menuAction, forbidClick} = data;
+        let {moldType, menuArr, menuAction, forbidClick} = data;
         forbidClick = forbidClick || [];
         if(!menuArr || menuArr.constructor !== Array){
             console.error('menuArr 不存在或不是一个Array！');
@@ -35,7 +36,11 @@ export default class MenuBar extends React.Component {
             console.error('menuAction 不存在或不是一个String！');
             return null;
         }
-        return {menuArr, menuAction, forbidClick};
+        if(!moldType || moldType.constructor !== String){
+            console.error('moldType 不存在或不是一个String！');
+            return null;
+        }
+        return {moldType, menuArr, menuAction, forbidClick};
     };
 
     // 菜单被点击
@@ -56,23 +61,43 @@ export default class MenuBar extends React.Component {
     };
 
     render() {
-        let {menuArr, menuAction, forbidClick} = this.state;
+        let {moldType, menuArr, menuAction, forbidClick} = this.state;
         return (
-            <div className={'menuBar'}>
-                {
-                    menuArr && menuArr.map(item => {
-                        return (
-                            <div
-                                key={item.key}
-                                onClick={this.handleMenuListClick.bind(this, item)}
-                                className={`menuBar_list ${menuAction === item.key ? 'menuBar_list_action' : ''} ${forbidClick.indexOf(item.key) !== -1 ? 'menuBar_list_not' : ''}`}
-                            >
-                                {item.zn_name}
-                            </div>
-                        )
-                    })
-                }
-            </div>
+            moldType === 'PC' ?
+                <div className={'menuBar_PC'}>
+                    {
+                        menuArr && menuArr.map(item => {
+                            return (
+                                <div
+                                    key={item.key}
+                                    onClick={this.handleMenuListClick.bind(this, item)}
+                                    className={`menuBar_list_PC ${menuAction === item.key ? 'menuBar_list_action_PC' : ''} ${forbidClick.indexOf(item.key) !== -1 ? 'menuBar_list_not_PC' : ''}`}
+                                >
+                                    {item.zn_name}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                :
+                moldType === 'Mobile' ?
+                    <div className={'menuBar_Mobile'}>
+                        {
+                            menuArr && menuArr.map(item => {
+                                return (
+                                    <div
+                                        key={item.key}
+                                        onClick={this.handleMenuListClick.bind(this, item)}
+                                        className={`menuBar_Mobile_list`}
+                                    >
+                                        <span className={`menuBar_Mobile_list_span ${menuAction === item.key ? 'menuBar_Mobile_list_span_active' : ''} ${forbidClick.indexOf(item.key) !== -1 ? 'menuBar_Mobile_list_span_not' : ''}`}>{item.zn_name}</span>
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                    :
+                    ''
         );
     }
 }
